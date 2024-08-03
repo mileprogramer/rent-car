a44d77795f7ec59cc29b66b325072f60ff43e17d
mileprogramer <obecano.ispunjeno@gmail.com>
2024-08-03 19:20:22 +0200

    add routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\RentedCarController;
+use App\Enums\AirConditionerType;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,16 +17,26 @@ use App\Http\Controllers\RentedCarController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('/test', function(){
-    $res = \App\Models\Car::all();
-    dd($res->toArray());
+    $res = \App\Enums\TransmissionType::values();
+    dd($res);
 });
 
 Route::get('cars', [CarController::class, 'index']);
+Route::get('cars/filter', [CarController::class, 'filter']);
 Route::get('cars/sold', [CarController::class, 'sold']);
 Route::get('cars/rented', [RentedCarController::class, 'index']);
 Route::get('cars/statistics', [RentedCarController::class, 'index']);
+Route::get('cars/broken', [RentedCarController::class, 'index']);
+Route::get('/car/edit/{car}', [CarController::class, 'edit']);
+Route::get('/car/show/{car}', [CarController::class, 'edit']);
+
+Route::post('/car/add', [CarController::class, 'store']);
+Route::post('/car/update/{id}', [CarController::class, 'update']);
+Route::post('/car/sell', [CarController::class, 'store']);
+Route::post('/car/rent', [CarController::class, 'store']);
+Route::post('/car/broke', [CarController::class, 'store']);
+Route::post('/car/rent/return', [CarController::class, 'store']);
+Route::post('/car/broke/return', [CarController::class, 'store']);
+
