2b4d5a4835ae7b417a947ff08b707058464f8228
mileprogramer <obecano.ispunjeno@gmail.com>
2024-11-05 22:03:41 +0100

    update the users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('phone', 20)->unique();
+            $table->string('phone', 20);
             $table->string('card_id', 20)->unique();
             $table->string('password');
             $table->rememberToken();
